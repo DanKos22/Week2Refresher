af74596933410b75c59efd871cc2d80e06dd321a
Dan Koskiranta <G00397054@atu.ie>
2024-09-25 20:12:11 +0100

    Updated code

--- src/main/java/ie/atu/week2_refresher/Product.java
+++ src/main/java/ie/atu/week2_refresher/Product.java
@@ -4,18 +4,20 @@ package ie.atu.week2_refresher;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
-    @Min(value = 1, message = "ID must be equal or greater to 1")
+    @Min(value = 1, message = "ID must be greater or equal to 1")
     private long id;
     @NotBlank(message = "Name cannot be blank")
     private String name;
-    @Min(value = 1, message = "Price must be equal or greater to 1")
+    @Positive(message = "Price must be a positive number")
     private double price;
 }
