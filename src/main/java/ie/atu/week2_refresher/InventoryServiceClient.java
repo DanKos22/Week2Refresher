c095cac88f9b5d13b71cc9a97946df30568f8bb1
Dan Koskiranta <G00397054@atu.ie>
2024-10-04 18:39:37 +0100

    Modified code

--- src/main/java/ie/atu/week2_refresher/InventoryServiceClient.java
+++ src/main/java/ie/atu/week2_refresher/InventoryServiceClient.java
@@ -5,9 +5,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "Inventory-Service", url = "http://inventoryservice:8081")
+@FeignClient(name = "Inventory-Service", url = "${feign.url}")
 public interface InventoryServiceClient {
     @PostMapping("/addProduct")
     //Product addProduct(@RequestBody Product product);
     String someDetails(@RequestBody Product product);
 }
+
+//http://inventoryservice:8081
